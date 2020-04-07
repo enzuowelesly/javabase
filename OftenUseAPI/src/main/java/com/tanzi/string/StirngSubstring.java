232457fbd6d7acda1ced80a52ba26647894c5696
tanzi <1184092238@qq.com>
2020-04-07 15:15:18 +0800

    javabase

---
+++ OftenUseAPI/src/main/java/com/tanzi/string/StirngSubstring.java
@@ -0,0 +1,11 @@
+package com.tanzi.string;
+
+public class StirngSubstring {
+
+    public static void main(String[] args) {
+        String str="javabase";
+        System.out.println(str);
+        System.out.println(str.substring(1));
+        System.out.println(str.substring(1,2));
+    }
+}
---
+++ OftenUseAPI/src/main/java/com/tanzi/string/StringChar.java
@@ -0,0 +1,13 @@
+package com.tanzi.string;
+
+public class StringChar {
+    public static void main(String[] args) {
+        String str1 ="Java base";
+        String str2="autotest";
+        System.out.println(str1.length());
+        System.out.println(str1.concat(str2));
+        System.out.println(str1.charAt(1));
+        System.out.println(str1.indexOf("b"));
+    }
+
+}
