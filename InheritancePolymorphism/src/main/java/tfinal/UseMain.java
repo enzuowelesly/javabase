e1a9263f1617639f9456a61ac7858e72172ee85c
tanzi <1184092238@qq.com>
2020-04-10 13:52:39 +0800

    javabase

--- InheritancePolymorphism/src/main/java/tfinal/UseMain.java
+++ InheritancePolymorphism/src/main/java/tfinal/UseMain.java
@@ -2,6 +2,9 @@ package tfinal;
 //final 修饰一个局部变量，只能赋值一次
 //基本数据类型存的数值，不能改变
 //引用数据类型存储的地址，不能改变，能改变的是通过调用方法改变值 如setter
+
+//final 修饰成员变量
+//声明时必须手动赋值
 public class UseMain {
     public static void main(String[] args) {
         final int num3=1;
---
+++ InheritancePolymorphism/src/main/java/tpermission/Permissions.java
@@ -0,0 +1,10 @@
+package tpermission;
+/**                  pulic    protected     空       private
+ 同一个类（我自己）       Y         Y          Y        Y
+ 同一个包 （我邻居）      Y         Y          Y        N
+ 不同包子类 （我儿子）    Y         Y          N        N
+ 不同包非子类（陌生人）    Y        N          N        N
+ **/
+ public class Permissions {
+
+}
