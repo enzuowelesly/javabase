4b5cb7dd47d1f80c3669e6f23e045e18ee023bfe
tanzi <1184092238@qq.com>
2020-04-10 13:08:04 +0800

    javabase

--- InheritancePolymorphism/src/main/java/tfinal/UseMain.java
+++ InheritancePolymorphism/src/main/java/tfinal/UseMain.java
@@ -1,5 +1,7 @@
 package tfinal;
 //final 修饰一个局部变量，只能赋值一次
+//基本数据类型存的数值，不能改变
+//引用数据类型存储的地址，不能改变，能改变的是通过调用方法改变值 如setter
 public class UseMain {
     public static void main(String[] args) {
         final int num3=1;
